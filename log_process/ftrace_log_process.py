a42cce3afc3fbe42e19141b373fab3fba8d7835c
Heyuan <hey_shi@yeah.net>
2017-01-14 15:18:40 +0800

    Update ftrace_log_process.py

--- log_process/ftrace_log_process.py
+++ log_process/ftrace_log_process.py
@@ -3,45 +3,39 @@ file_object = open("ping_sched.txt")
 # 查看进程名
 process_id = "ping-10105"
 # 查看时间段
-timestamp_start = 48479.058831
-timestamp_end = 48479.951749
+timestamp_start = 0
+timestamp_end = 50000
 # 统计耗时
-statistics = {'sched_stat_runtime': 0, 'sched_stat_sleep': 0, 'sched_stat_wait': 0}
+# statistics = {'sched_stat_runtime': 0, 'sched_stat_sleep': 0, 'sched_stat_wait': 0,
+#               'sched_stat_blocked':0,'sched_stat_iowait':0}
+stat_cost = {}
 # 百分比
-percentage = {'sched_stat_runtime': 0, 'sched_stat_sleep': 0, 'sched_stat_wait': 0}
+# percentage = {'sched_stat_runtime': 0, 'sched_stat_sleep': 0, 'sched_stat_wait': 0}
+stat_percent ={}
 
 for ftace_log in file_object.readlines():
     # print(ftace_log,end="")
     column =ftace_log.split(" ")
     # remove blank component, get pure log data
-    list = []
+    info_list = []
     for atom in column:
         if len(atom)!=0:
-            list.append(atom)
+            info_list.append(atom)
     # find target process id and get time cost
-    timestamp = float(list[3].replace(':',''))
-    if list[0] == process_id and timestamp >= timestamp_start and timestamp <= timestamp_end:
-        # print(list)
-        if list[4] == 'sched_stat_runtime:':
-            # print(list)
-            runtime = list[7].split('=')
-            statistics['sched_stat_runtime'] += int( runtime[1])
-        elif list[4] == 'sched_stat_sleep:':
-            # print(list)
-            sleep = list[7].split('=')
-            statistics['sched_stat_sleep'] += int(sleep[1])
-        elif list[4] == 'sched_stat_wait:':
-            # print(list)
-            wait = list[7].split('=')
-            statistics['sched_stat_wait'] += int(wait[1])
-        else:
-            pass
-print(statistics)
+    timestamp = float(info_list[3].replace(':',''))
+    if info_list[0] == process_id and timestamp >= timestamp_start and timestamp <= timestamp_end:
+        if 'sched_stat' in info_list[4]:
+            time_cost = float(info_list[7].split('=')[1])
+            if info_list[4] in stat_cost:
+                stat_cost[info_list[4]] += time_cost
+            else:
+                stat_cost[info_list[4]] = time_cost
 # get percentage of each schedule state
 time_sum = 0
-for key in statistics:
-    time_sum += statistics[key]
-percentage['sched_stat_runtime'] = statistics['sched_stat_runtime']/time_sum
-percentage['sched_stat_sleep'] = statistics['sched_stat_sleep'] / time_sum
-percentage['sched_stat_wait'] = statistics['sched_stat_wait'] / time_sum
-print(percentage)
+for key in stat_cost:
+    time_sum += stat_cost[key]
+for key in stat_cost:
+    stat_percent[key] = stat_cost[key] / time_sum
+# output
+print(stat_cost)
+print( stat_percent)
