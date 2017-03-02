7b88293c4aea94e896520d8dca1efc06ec7f7280
xhl2791188 <847036983@qq.com>
2017-03-02 15:17:44 +0800

    update GT

---
+++ projects/android-log/GT/android/sdk/test/test/tencent/wstt/gt/client/DemoApplication.java
@@ -0,0 +1,11 @@
+package test.tencent.wstt.gt.client;
+
+import android.app.Application;
+
+public class DemoApplication extends Application {
+	
+	@Override
+	public void onCreate() {
+		super.onCreate();
+	}
+}
