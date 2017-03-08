f5121248207ec07005586f7956c72c89022b4cfb
weikang1993 <840227400@qq.com>
2017-03-08 17:07:50 +0800

    Delete openSchedEvents.sh

--- projects/kernel-log/log_process/help/openSchedEvents.sh
+++
@@ -1,8 +0,0 @@
-#clean trace record
-echo 0 > /sys/kernel/debug/tracing/trace
-#resize buffer
-echo 102400 > /sys/kernel/debug/tracing/buffer_size_kb
-#enable sched events
-echo 1 > /sys/kernel/debug/tracing/events/sched/enable
-#enable trace
-echo 1 > /sys/kernel/debug/tracing/tracing_on
