30c4578b578f9b28b9a1798696ad225f4db22499
weikang1993 <840227400@qq.com>
2017-03-08 17:07:34 +0800

    Delete closeSchedEvents.sh

--- projects/kernel-log/log_process/help/closeSchedEvents.sh
+++
@@ -1,8 +0,0 @@
-#close trace
-echo 0 > /sys/kernel/debug/tracing/tracing_on
-#save record
-cat /sys/kernel/debug/tracing/trace > $1.full_log
-#save selected pid record
-cat /sys/kernel/debug/tracing/trace | grep $1- > $1.select_log
-#clean trace recode
-echo 0 > /sys/kernel/debug/tracing/trace
